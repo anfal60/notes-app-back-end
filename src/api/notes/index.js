56a53ee119a9996adf4218cd0fcaa198ae6e7a4f
Anas <anfal60@gmail.com>
2022-03-12 20:23:43 +0700

    restructure with hapi plugin

---
+++ src/api/notes/index.js
@@ -0,0 +1,14 @@
+//const { server } = require("@hapi/hapi");
+//const { route } = require("@hapi/hapi/lib/cors");
+const NotesHandler = require('./handler');
+const routes = require('./routes');
+
+module.exports = {
+    name: 'notes',
+    version: '1.0.0',
+    register: async (server, { service }) => {
+        const notesHandler = new NotesHandler(service);
+        server.route(routes(notesHandler));
+    },
+
+};
